426493261ac5c90f2a2e115c84626de7ebc8aaf1
suleman <marsuleman@hotmail.com>
2020-06-24 11:15:37 -0400

    first commit

---
+++ src/main/java/com/jaococogradle/jacocogradlereport/JacocogradlereportApplication.java
@@ -0,0 +1,13 @@
+package com.jaococogradle.jacocogradlereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocogradlereportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacocogradlereportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaococogradle/jacocogradlereport/JacocogradlereportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaococogradle.jacocogradlereport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocogradlereportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
